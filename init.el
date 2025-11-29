01972ca7b8114132f8ec8a61801f425da88e2aef
hys <jmmirsberger@gmail.com>
2025-11-29 16:48:35 -0600

    sets up melpa package archive and adds magit

---
+++ init.el
@@ -0,0 +1,11 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package magit
+  :ensure t)
