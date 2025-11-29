9a8b0a2047d758f1536828722b8fed6363c9778d
hys <jmmirsberger@gmail.com>
2025-11-29 17:10:57 -0600

    binds C-x C-b to open in current buffer

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(tool-bar-mode -1)
+(menu-bar-mode 1)
+
+;; buffer menu will display in current buffer window
+(global-set-key "\C-x\C-b" 'buffer-menu)
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
